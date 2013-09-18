243cfd9fbe9a11feee5cffbfec5bd4c6632f1c09
sandeep2357 <kumarsandeep2357@gmail.com>
2013-09-18 16:39:07 +0530

    Update filterKernel.py

--- Tests/filterKernel.py
+++ Tests/filterKernel.py
@@ -1,4 +1,3 @@
-
 from gvsig import *
 from gvsig_raster import *
 from geom import *
@@ -22,7 +21,6 @@ def main():
   fileName='C:\Users\Sandeep\Desktop\Costa1.tif'
   layer = loadRasterLayer(fileName)
   layer.filterKernel(smoothFilter,"new3.jpg", Buffer.TYPE_BYTE, 3)
-  # replace the filepath with appropriate file
   # replace the filepath with appropriate file
   # careful with the \\ before n. As '\n' makes a newline
   # and gives wrong filename, we used '\\'
